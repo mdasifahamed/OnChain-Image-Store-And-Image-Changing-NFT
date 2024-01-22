89d3db77062c0f01db72b4f0d86e34748d27b73d
mdasifahamed <mdasifahamed89@yahoo.com>
2024-01-22 20:17:32 +0600

    DeployMnet Script Added

---
+++ script/DeployDrive.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import{Script} from "forge-std/Script.sol";
+
+import{Drive} from "../src/Drive.sol";
+
+contract DeployDrive is Script{
+
+    function run() external {
+        uint deprivate = vm.envUint("PRIVATEKEY");
+
+        string memory classicImageSvg = vm.readFile("./image_svgs/Classic.svg");
+
+        string memory SportsImageSvg = vm.readFile("./image_svgs/Sports.svg");
+
+        vm.startBroadcast(deprivate);
+        
+        Drive(svgToImgUri(classicImageSvg),svgToImgUri(SportsImageSvg));
+
+        vm.stopBroadcast();
+    }
+
+    function svgToImgUri(string memory svgimg) internal pure returns(string memory){
+
+        string memory baseURI = "data:image/svg+xml;base64,";
+
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svgimg))) 
+        );
+
+        return string(abi.encodePacked(baseURI, svgBase64Encoded));
+    }
+
+}
